4024e4b236681ef94b402a1f4cf5bf8b1fda9edd
tianrui <haitongjie@qq.com>
2020-08-28 20:44:36 +0800

    refactor: refactor delivery.js

--- src/delivery.js
+++ src/delivery.js
@@ -1,38 +1,78 @@
-function deliveryDate(anOrder, isRush) {
-    if (isRush) {
-        let deliveryTime;
-        if ([
-            'MA',
-            'CT',
-        ].includes(anOrder.deliveryState)) {
-            deliveryTime = 1;
-        } else if ([
-            'NY',
-            'NH',
-        ].includes(anOrder.deliveryState)) {
-            deliveryTime = 2;
-        } else {
-            deliveryTime = 3;
-        }
-        return anOrder.placedOn.plusDays(1 + deliveryTime);
-    } else {
-        let deliveryTime;
-        if ([
-            'MA',
-            'CT',
-            'NY',
-        ].includes(anOrder.deliveryState)) {
-            deliveryTime = 2;
-        } else if ([
-            'ME',
-            'NH',
-        ].includes(anOrder.deliveryState)) {
-            deliveryTime = 3;
-        } else {
-            deliveryTime = 4;
-        }
-        return anOrder.placedOn.plusDays(2 + deliveryTime);
+const DeliveryStateA = [
+    'MA',
+    'CT',
+];
+
+function isDeliveryStateInDeliveryStateA(anOrder) {
+    return DeliveryStateA.includes(anOrder.deliveryState);
+}
+
+const DeliveryStateB = [
+    'NY',
+    'NH',
+];
+
+function isDeliveryStateInDeliveryStateB(anOrder) {
+    return DeliveryStateB.includes(anOrder.deliveryState);
+}
+
+function getDeliveryTimeWhenIsRush(anOrder) {
+    if (isDeliveryStateInDeliveryStateA(anOrder)) {
+        return 1;
     }
+    if (isDeliveryStateInDeliveryStateB(anOrder)) {
+        return 2;
+    }
+    return 3;
+}
+
+const DeliveryStateC = [
+    'MA',
+    'CT',
+    'NY',
+];
+
+function isDeliveryStateInDeliveryStateC(anOrder) {
+    return DeliveryStateC.includes(anOrder.deliveryState);
+}
+
+const DeliveryStateD = [
+    'ME',
+    'NH',
+];
+
+function isDeliveryStateInDeliveryStateD(anOrder) {
+    return DeliveryStateD.includes(anOrder.deliveryState);
+}
+
+function getDeliveryTimeB(anOrder, deliveryTime) {
+    if (isDeliveryStateInDeliveryStateC(anOrder)) {
+        deliveryTime = 2;
+    } else if (isDeliveryStateInDeliveryStateD(anOrder)) {
+        deliveryTime = 3;
+    } else {
+        deliveryTime = 4;
+    }
+    return deliveryTime;
+}
+
+function getDeliveryDateWhenIsRush(anOrder) {
+    let deliveryTime;
+    deliveryTime = getDeliveryTimeWhenIsRush(anOrder);
+    return anOrder.placedOn.plusDays(1 + deliveryTime);
+}
+
+function getDeliveryTime(anOrder) {
+    let deliveryTime;
+    deliveryTime = getDeliveryTimeB(anOrder, deliveryTime);
+    return anOrder.placedOn.plusDays(2 + deliveryTime);
+}
+
+function deliveryDate(anOrder, isRush) {
+    return isRush
+        ? getDeliveryDateWhenIsRush(anOrder)
+        : getDeliveryTime(anOrder);
+
 }
 
 module.exports = {
